75e4d9067d651368ec03d5988905975dab40f4dc
AapoKorhonen <aapo.korhonen23@gmail.com>
2026-06-23 11:20:15 +0300

    updating functions

--- R/empirical_target_matrix.R
+++ R/empirical_target_matrix.R
@@ -8,7 +8,8 @@ select_empirical_target_matrix <- function(data, number_or_groups,n, memory_save
     comp_data <- rbind(comp_data,data[[i]])
   }
   #if(scale_data) comp_data <- scale(comp_data)
-  
+  if(memory_save){ gc()}
+  comp_data <- na.omit(comp_data)
   if(memory_save){ gc()}
   est <- cvCovEst::linearShrinkLWEst(comp_data)
   if(memory_save){ gc()}
--- R/sample_covariance_cal.R
+++ R/sample_covariance_cal.R
@@ -3,8 +3,6 @@ sample_covariance_cal <- function(data_matrix){
   
   n <- nrow(data_matrix)
   
-  data_matrix <- na.omit(data_matrix)
-  
   X_centered <- scale(data_matrix, center = T, scale = F)
   
   S <- t(X_centered) %*% X_centered / (n-1)
